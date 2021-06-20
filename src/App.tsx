70ec99669a57c8efcee3f5151a8ef2a9256627f5
hoon <iamhoonjeong@gmail.com>
2021-06-20 11:01:57 +0900

    [add] react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
 
 import GlobalStyle from './styles/globalStyle';
 import './styles/App.scss';
@@ -38,6 +39,20 @@ function App() {
 
   return (
     <>
+      <Helmet>
+        <link
+          rel="icon"
+          href="%PUBLIC_URL%/favicon.svg"
+          sizes="any"
+          type="image/svg+xml"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="%PUBLIC_URL%/favicon.svg"
+          sizes="180x180"
+          type="image/svg+xml"
+        />
+      </Helmet>
       <GlobalStyle />
       {splash ? (
         <Splash splash onChangeSplash={onChangeSplash} />
